3b6186c0a4cd11f41ce7f1b70246a11320f6c2c9
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-04 10:51:08 -0500

    Created UI and initial functions

---
+++ app.js
@@ -0,0 +1,37 @@
+let number1; //change these to const?
+let number2;
+let operator;
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;      
+    }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
