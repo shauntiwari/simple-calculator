7f9de67fb539d96fe82fa1d2b6a870c2b12dad51
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-04 13:22:26 -0500

    Basic functionality completed

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
-let number1; //change these to const?
-let number2;
-let operator;
+let currentInput = 0; //change these to const?
+let previousInput = 0;
+let calcResult = 0;
+let operator = '';
 
-function operate(num1, num2, operator) {
-    switch (operator) {
+const display = document.getElementById("display");
+
+function clearAll() {
+    display.value = '';
+    currentInput = 0;
+    previousInput = 0;
+    operator = '';
+}
+
+function numberInput(char) {
+    display.value += char;
+}
+
+function operatorInput(char) {
+    previousInput = parseInt(display.value);
+    operator = char;
+    display.value = '';
+}
+
+function calculate() {
+    currentInput = parseInt(display.value);
+    
+    if (currentInput === 0 && operator === '÷') {
+        calcResult = 'Cannot divide by zero!';
+    }
+    else {
+        calcResult = operate(previousInput, currentInput, operator);
+    }
+    
+    display.value = calcResult;
+}
+
+function operate(num1, num2, oper) {
+    switch (oper) {
         case '+':
             return add(num1, num2);
             break;
         case '-':
             return subtract(num1, num2);
             break;
-        case '*':
+        case 'x':
             return multiply(num1, num2);
             break;
-        case '/':
+        case '÷':
             return divide(num1, num2);
             break;      
     }
@@ -34,4 +67,3 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
-
